249d5274fcbeeb7504e284936c94501d3234fe62
ganesh19gani <97647488+ganesh19gani@users.noreply.github.com>
2023-03-28 14:49:28 +0530

    RabbitMQ complete Producer and Consumer c hanges

---
+++ src/main/java/com/ojas/app/controller/MessageController.java
@@ -0,0 +1,28 @@
+package com.ojas.app.controller;
+
+import com.ojas.app.publisher.RabbitMQProducer;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageController {
+
+    private RabbitMQProducer producer;
+
+    public MessageController(RabbitMQProducer producer) {
+        this.producer = producer;
+    }
+
+    @GetMapping("/publish")
+    public ResponseEntity<String> sendMessage(@RequestParam("message") String message) {
+
+        producer.sendMessage(message);
+        return ResponseEntity.ok("Message sent to RabbitMQ ...");
+    }
+
+}
